e0c93eea77de92656e214edab6e955af7dc2bbc6
김승연 <72969123+sssseungk@users.noreply.github.com>
2023-11-28 16:04:42 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,10 +1,14 @@
 function solution(array) {
     var answer = 0;
+
     // 배열 오름차순 정렬
     array.sort(function(a, b) {
         return a - b;
     });
+
+    // 배열 중앙 인덱스 값 구하기 
     let index = Math.floor(array.length/2);
+    // 배열의 중앙값을 answer에 저장
     answer = array[index];
     return answer;
 }
