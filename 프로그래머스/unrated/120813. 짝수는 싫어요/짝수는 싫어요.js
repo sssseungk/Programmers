286e17cb6191f88c079c74a0b19ac72b843cd28f
김승연 <72969123+sssseungk@users.noreply.github.com>
2023-11-28 16:21:20 +0900

    [level unrated] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,16 +1,7 @@
 function solution(n) {
     let answer = [];
-    // n이 짝수일 경우
-    if(n % 2 !== 0){
-        for(let i=1; i<=n; i+=2){
-            answer.push(i);
-        }
-    }
-    // n이 홀수일 경우
-    else{
-        for(let i=1; i<=n; i+=2){
-            answer.push(i);
-        }
+    for(let i=1; i<=n; i+=2){
+        answer.push(i);
     }
     return answer;
 }
