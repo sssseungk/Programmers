a6fa2e5a25e40185f512fed026df1838f678162a
김승연 <72969123+sssseungk@users.noreply.github.com>
2023-12-06 15:27:22 +0900

    [level unrated] Title: 공 던지기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/unrated/120843. 공 던지기/공 던지기.js
@@ -3,7 +3,8 @@ function solution(numbers, k) {
     // k번째 공 던지는 사람의 배열의 인덱스 => 2 * (k - 1)
     // 배열의 인덱스는 0부터 시작이므로 -1 해줘야함.
     
-    // 배열 길이를 초과했을 땐 다시 맨 처음으로 돌아가서 계산해야함
+    // % numbers.length 를 통해 배열의 길이를 초과할 경우 처음으로 돌아가게 됨
+    // k번째 공 던진 사람의 위치 찾는 식
     let index = (2 * (k-1)) % numbers.length;
     answer = numbers[index];
     
