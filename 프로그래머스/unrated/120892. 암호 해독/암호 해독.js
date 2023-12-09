abc1b50af3268021a5122079f883978a462386ba
김승연 <72969123+sssseungk@users.noreply.github.com>
2023-12-09 20:35:53 +0900

    [level unrated] Title: 암호 해독, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/unrated/120892. 암호 해독/암호 해독.js
@@ -2,8 +2,8 @@ function solution(cipher, code) {
     let answer = [];
     let array = cipher.split("")
 
-    for(let i=0; i<=array.length; i+=code){
-        answer.push(array[i-1]);
+    for(let i=code-1; i<array.length; i+=code){
+        answer.push(array[i]);
     }
     return answer.join("");
 }
