12581ce485e76e7bbf278e2ff38ad843485555dc
김승연 <72969123+sssseungk@users.noreply.github.com>
2023-12-10 15:37:33 +0900

    [level unrated] Title: 한 번만 등장한 문자, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -2,11 +2,11 @@ function solution(s) {
     let array = [];
     
     for(let i=0; i<s.length; i++){
-        if(!array.includes(s[i])){
-            // 문자열에 해당 문자가 하나만 존재하는지 확인해야함
-            if(s.split(s[i]).length-1 === 1){
+        // 문자열에 해당 문자가 하나만 존재하는지 확인해야함
+        if(s.split(s[i]).length-1 === 1){
+            if(!array.includes(s[i])){
                 array.push(s[i]);
-            }
+            } 
         }
     }
     
