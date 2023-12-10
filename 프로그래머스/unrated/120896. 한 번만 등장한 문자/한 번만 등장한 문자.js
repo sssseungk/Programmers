e636e32e9a3622166721de3346ed7bd5e388e21e
김승연 <72969123+sssseungk@users.noreply.github.com>
2023-12-10 15:33:12 +0900

    [level unrated] Title: 한 번만 등장한 문자, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    let array = [];
+    
+    for(let i=0; i<s.length; i++){
+        if(!array.includes(s[i])){
+            // 문자열에 해당 문자가 하나만 존재하는지 확인해야함
+            if(s.split(s[i]).length-1 === 1){
+                array.push(s[i]);
+            }
+        }
+    }
+    
+    // 사전순으로 정렬
+    array.sort();
+    return array.join("");
+}
